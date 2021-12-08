02f37977daef8d31260e3aeeef8c99c2f4436feb
Philipp Gayret <philipp@hileco.com>
2021-12-08 14:43:34 +0100

    Added data file loading for json and yml

--- main.py
+++ main.py
@@ -3,29 +3,22 @@ import jinja2
 import json
 import sys
 import markupsafe
+import yaml
 
 sys.stdout.reconfigure(encoding='utf-8')
 sys.stderr.reconfigure(encoding='utf-8')
 
-if len(sys.argv) < 2:
-    print(f'Usage: {sys.argv[0]} <template> <json file 1> ... <json file N>', file=sys.stderr)
+if len(sys.argv) < 3:
+    print(f'Usage: {sys.argv[0]} <template> <github context>', file=sys.stderr)
     exit(1)
 
-jinja_loader = jinja2.FileSystemLoader('./')
-jinja_environment = jinja2.Environment(loader=jinja_loader)
-jinja_environment.globals['include_file'] = lambda name: markupsafe.Markup(jinja_loader.get_source(jinja_environment, name)[0])
-jinja_template = jinja_environment.get_or_select_template(sys.argv[1])
+j2loader = jinja2.FileSystemLoader('./')
+j2environment = jinja2.Environment(loader=j2loader)
+j2environment.globals['load'] = lambda name: markupsafe.Markup(j2loader.get_source(j2environment, name)[0])
+j2environment.globals['load_json'] = lambda name: json.loads(j2loader.get_source(j2environment, name)[0])
+j2environment.globals['load_yaml'] = lambda name: yaml.safe_load(j2loader.get_source(j2environment, name)[0])
+j2template = j2environment.get_or_select_template(sys.argv[1])
+github = json.loads(sys.argv[2])
 
-data = {}
-for i in range(2, len(sys.argv)):
-    file_path = sys.argv[i]
-    with open(file_path, 'r', encoding='utf-8') as f:
-        file_content = f.read()
-        file_name_ext = file_path.split('/')[-1]
-        if file_name_ext.endswith('.json'):
-            file_name = file_name_ext.split('.')[0]
-            file_data = json.loads(file_content)
-            data[file_name] = file_data
-
-rendered = jinja_template.render(data)
+rendered = j2template.render(github=github)
 print(rendered)
