1b5321ccd02e4d40602313d9823ff1accd2cda1b
Philipp Gayret <philipp@hileco.com>
2021-12-10 00:17:17 +0100

    Added Markdown TOC parsing

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 import sys
 import markupsafe
 import yaml
+import markdown
 
 sys.stdout.reconfigure(encoding='utf-8')
 sys.stderr.reconfigure(encoding='utf-8')
@@ -12,11 +13,27 @@ if len(sys.argv) < 3:
     print(f'Usage: {sys.argv[0]} <template> <github context>', file=sys.stderr)
     exit(1)
 
+
+def load_markdown_toc(text):
+    md = markdown.Markdown(extensions=['toc'])
+    md.convert(text)
+    return md.toc
+
+
+def load_markdown_toc_tokens(text):
+    md = markdown.Markdown(extensions=['toc'])
+    md.convert(text)
+    return md.toc_tokens
+
+
 j2loader = jinja2.FileSystemLoader('./')
 j2environment = jinja2.Environment(loader=j2loader)
 j2environment.globals['load'] = lambda name: markupsafe.Markup(j2loader.get_source(j2environment, name)[0])
 j2environment.globals['load_json'] = lambda name: json.loads(j2loader.get_source(j2environment, name)[0])
 j2environment.globals['load_yaml'] = lambda name: yaml.safe_load(j2loader.get_source(j2environment, name)[0])
+j2environment.globals['load_markdown_toc'] = lambda text: load_markdown_toc(j2loader.get_source(j2environment, text)[0])
+j2environment.globals['load_markdown_toc_tokens'] = lambda text: \
+    load_markdown_toc_tokens(j2loader.get_source(j2environment, text)[0])
 j2template = j2environment.get_or_select_template(sys.argv[1])
 github = json.loads(sys.argv[2])
 
---
+++ sample/data.md
@@ -0,0 +1,13 @@
+# Header One
+
+## Header Two
+
+Content 1
+
+### Header Three
+
+Content 2
+
+### Header Three, Two
+
+Content 3
